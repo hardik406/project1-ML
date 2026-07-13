40a17ba2d12034843529559b0581079fff786493
hardik406 <hardiklamboria1@gmail.com>
2026-07-13 16:59:25 +0530

    Completed end-to-end ML project with Flask and hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -65,3 +65,13 @@ def evaluate_models(
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
